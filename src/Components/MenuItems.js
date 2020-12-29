f6fbafba949b8f1028bfcc0692da1b96b6526d42
keanu-natchev <keanu.natchev@mail.mcgill.ca>
2020-12-28 23:44:24 -0500

    Working on the navbar.

---
+++ src/Components/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'MEN',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'ALL',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'WOMEN',
+        url: '#',
+        cName: 'nav-links'
+    },
+]
---
+++ src/Components/Navbar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { MenuItems } from './MenuItems';
+import './Navbar.css';
+
+class Navbar extends Component {
+    state = {
+        burger: false,
+        mobileWindow: false,
+        windowSize: window.innerWidth
+    };
+
+    handleBurgerClick = (burger, mobileWindow, windowSize) => {
+        if(state.burger) {
+            this.setState({ burger: true });
+        }
+        // burger = !burger;
+        // this.setState({ burger: !this.state.burger });
+    }
+
+    render() {
+        return (
+            <>
+                <nav className='NavbarItems'>
+                    <h1 className='brand-logo'>Brand</h1>
+                    <ul className={(this.state.burger) ? 'menu-open' : 'menu' }>
+                        {MenuItems.map((item, index) => {
+                            return (
+                                <li className='menu-li' key={index}>
+                                    <a className='menu-a' href={item.url}>
+                                        {item.title}
+                                    </a>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    <div className='burger' onClick={this.handleBurgerClick}>
+                        <div className='burger-slice'></div>
+                        <div className='burger-slice'></div>
+                        <div className='burger-slice'></div>
+                    </div>
+                </nav>
+            </>
+        )
+    }
+}
+
+export default Navbar;
