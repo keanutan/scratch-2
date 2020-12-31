eeee9b8da84972d1c6ac6c5105a25a31bec2899f
keanu-natchev <keanu.natchev@mail.mcgill.ca>
2020-12-30 19:48:22 -0500

    Frontend development for eventregistration project.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import FrontEnd from './Components/FrontEnd';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <FrontEnd />
     </div>
   );
 }
---
+++ src/Components/FrontEnd.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import './FrontEnd.css';
+
+class FrontEnd extends Component {
+    state = {
+        burger: false,
+        mobileWindow: false,
+        windowSize: window.innerWidth
+    };
+
+    handleBurgerClick = () => {
+        this.setState({ burger: !this.state.burger });
+    }
+
+    render() {
+        return (
+            <>
+                <div className='row'>
+                    <h3>Persons</h3>
+                    <div className='column'>
+                        <div className='Name'>
+                            <h4>Name</h4>
+                            <input type='text' className='name-input' placeholder='Person Name'></input>
+                        </div>
+                    </div>
+                    <div className='column'>
+                        <h4>Events</h4>
+                        <select>
+                            <option>Person</option>
+                            <option>Artist</option>
+                        </select>
+                    </div>
+                    <div className='column'>
+                        <h4>Payment ID</h4>
+                        <button>Create Person</button>
+                    </div>
+                    <div className='column'>
+                        <h4>Amount ($)</h4>
+                    </div>
+                </div>
+                <div className='row'>
+                    <h3>Events</h3>
+                    <div className='column'>
+                        <div className='Name'>
+                            <h4>Name</h4>
+                            <input type='text' className='name-input' placeholder='Event Name'></input>
+                        </div>
+                    </div>
+                    <div className='column'>
+                        <div className='Name'>
+                            <h4>Date</h4>
+                            <input type='date' className='name-input' placeholder='Event Name'></input>
+                        </div>
+                    </div>
+                    <div className='column'>
+                        <div className='Name'>
+                            <h4>Start</h4>
+                            <input type='time' className='name-input' placeholder='Event Name'></input>
+                        </div>
+                    </div>
+                    <div className='column'>
+                        <div className='Name'>
+                            <h4>End</h4>
+                            <input type='time' className='name-input' placeholder='Event Name'></input>
+                        </div>
+                    </div>
+                    <div className='column'>
+                        <div className='Name'>
+                            <h4>Artist</h4>
+                            <input type='text' className='name-input' placeholder='Event Artist'></input>
+                        </div>
+                    </div>
+                </div>
+                <div className='row'>
+                    <h3>Registrations</h3>
+                    <div className='column'>
+                        <h4>Person: </h4>
+                        <select>
+                            <option>Please select one</option>
+                            <option>B</option>
+                            <option>C</option>
+                        </select>
+                    </div>
+                    <div className='column'>
+                        <h4>Event: </h4>
+                        <select>
+                            <option>Please select one</option>
+                            <option>B</option>
+                            <option>C</option>
+                        </select>
+                    </div>
+                    <div className='column'>
+                        <h4>Register:</h4>
+                        <button>Register</button>
+                    </div>
+                </div>
+                <div className='row'></div>
+                <div className='row'></div>
+            </>
+        )
+    }
+}
+
+export default FrontEnd;
